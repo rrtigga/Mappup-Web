1d1820217d5a0a1f8204d7a6b18c84fd4a70a746
rrtigga <rrtigga@ucdavis.edu>
2016-02-15 04:12:54 -0800

    added direction button

--- TODO.md
+++ TODO.md
@@ -1,1 +1,9 @@
-TODO.md
+-Load Markers from Firebase 
+
+-Load different infobubbles in each marker
+
+-Make Map Responsive 
+
+-Figure out how to scrape all facebook events by city
+
+-Get Directions with Google Maps API
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var Firebase = require("firebase");
+
+
+var myFirebaseRef = new Firebase("https://hittup.firebaseio.com/");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
